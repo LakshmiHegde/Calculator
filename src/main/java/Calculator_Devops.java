c09ad464e8e164fc0fc41196ec947fd1a250397c
LakshmiHegde <lakshmi.hegde777@gmail.com>
2022-04-02 23:55:23 +0530

    changes committed

--- src/main/java/Calculator_Devops.java
+++ src/main/java/Calculator_Devops.java
@@ -1,15 +1,21 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Calculator_Devops {
+    private static final Logger logger = LogManager.getLogger(Calculator_Devops.class);
     public static void main(String[] args)
     {
         System.out.println("----Calculator----");
+        logger.info("------Calculator------");
         while(true)
         {
             System.out.println("\n\n1. Square root function\n2. Factorial function\n3. Natural logarithm(base e)\n4. Power function\n5. Exit\n");
             System.out.println("Your choice:\t");
             Scanner sc=new Scanner(System.in);
             int choice=sc.nextInt();
+            logger.info("Choice entered : "+choice);
             Calculator_Devops c=new Calculator_Devops();
             switch (choice)
             {
@@ -38,21 +44,33 @@ public class Calculator_Devops {
     }
     public double squareroot(double number)
     {
-        return Math.sqrt(number);
+        logger.info("Called square root, input number: "+number);
+        double result=Math.sqrt(number);
+        logger.info("Result: "+result);
+        return result;
+
     }
     public double factorial(double number)
     {
+        logger.info("Called Factorial, input number: "+number);
         double fact=1;
         for(double i=2;i<=number;i++)
             fact*=i;
+        logger.info("Result: "+fact);
         return fact;
     }
     public double logarithm(double number)
     {
-        return Math.log(number);
+        logger.info("Called logarithm, input number: "+number);
+        double result=Math.log(number);
+        logger.info("Result: "+result);
+        return result;
     }
     public double power(double a,double b)
     {
-        return Math.pow(a,b);
+        logger.info("Called power , input number: "+a+" "+b);
+        double result=Math.pow(a,b);
+        logger.info("Result: "+result);
+        return result;
     }
 }
