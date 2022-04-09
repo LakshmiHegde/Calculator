bcb14b552f7542cc94548fd50ed7733e79d54721
Lakshmi Hegde <44948174+LakshmiHegde@users.noreply.github.com>
2022-04-09 21:13:41 +0530

    Update Calculator_Devops.java

--- src/main/java/Calculator_Devops.java
+++ src/main/java/Calculator_Devops.java
@@ -7,7 +7,6 @@ public class Calculator_Devops {
     private static final Logger logger = LogManager.getLogger(Calculator_Devops.class);
     public static void main(String[] args)
     {
-        System.out.println("Enterd\n");
         System.out.println("----Calculator----");
         logger.info("------Calculator------");
         while(true)
